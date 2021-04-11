5832f074b1888b8dc4b849f212e547899148a9b1
Siddhant Jaiswal <34509466+Hades24@users.noreply.github.com>
2021-04-11 18:52:24 +0530

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import * as firebase from 'firebase/app'
+import firebaseConfig from './config/environment'
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp(firebaseConfig);
+  }
 }).$mount('#app')
