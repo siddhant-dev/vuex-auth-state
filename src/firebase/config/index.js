a8c338da1ffecb44843f4bcb1f5460d0c7216460
Siddhant Jaiswal <34509466+siddhant-dev@users.noreply.github.com>
2021-04-12 16:03:42 +0530

    🛠 config

---
+++ src/firebase/config/index.js
@@ -0,0 +1,11 @@
+export default {
+    firebaseConfig : {
+        apiKey: <Your-Key>,
+        authDomain: "cloudfirestorelearning.firebaseapp.com",
+        databaseURL: "https://cloudfirestorelearning.firebaseio.com",
+        projectId: "cloudfirestorelearning",
+        storageBucket: "cloudfirestorelearning.appspot.com",
+        messagingSenderId: "681859861309",
+        appId: "1:681859861309: web:1726f8a46bf9b5b48a9354"
+    }
+}
